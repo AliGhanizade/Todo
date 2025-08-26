91a1ce8579e57daeeae6895e51b9bb503a155b73
AliGhanizade <thealighanizade@gmail.com>
2025-08-26 22:52:27 +0330

    create func task and save to json file

---
+++ todo/create.go
@@ -0,0 +1,34 @@
+package todo
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+	"time"
+)
+
+func Create() {
+	var task Task
+
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Print("Enter title: ")
+	title, _ := reader.ReadString('\n')
+	task.Title = strings.TrimSpace(title) // Remove leading/trailing whitespace and newline
+
+	fmt.Print("Enter description: ")
+	description, _ := reader.ReadString('\n')
+	task.Description = strings.TrimSpace(description)
+	
+	task.IsCompleted = false
+	task.CreateTime = time.Now().Format(time.DateTime)
+
+	task.ID = LenTasks() + 1
+
+	err := SaveTasksToFile(task)
+	if err != nil {
+		fmt.Print(err)
+		return
+	}
+}
