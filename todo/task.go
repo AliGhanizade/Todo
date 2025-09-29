aa64327bd4c32e68a35a479c7025b9ff934af354
AliGhanizade <thealighanizade@gmail.com>
2025-09-29 20:37:35 +0330

    delete file for change dir path or not used for save data

--- todo/task.go
+++
@@ -1,9 +0,0 @@
-package todo
-
-type Task struct {
-	ID          int    `json:"id"`
-	Title       string `json:"title"`
-	Description string `json:"description"`
-	IsCompleted bool   `json:"id_completed"`
-	CreateTime  string `json:"created_time"`
-}
