d3a368da8d3ec63c65c957285d61565c81a3efdc
AliGhanizade <thealighanizade@gmail.com>
2025-09-29 20:42:06 +0330

    delete data from DB by id

---
+++ task/delete.go
@@ -0,0 +1,29 @@
+package task
+
+import (
+	"net/http"
+	"todo/share"
+	"todo/model"
+
+
+	"github.com/gin-gonic/gin"
+)
+
+func (t *TaskController) Delete(ctx *gin.Context) {
+	var task model.Task
+	var uri share.TaskURI
+
+	err := ctx.ShouldBindUri(&uri)
+	if err != nil {
+		share.NewError(http.StatusBadRequest, err.Error(), ctx)
+		return
+	}
+
+	err = task.Delete(uri.ID)
+	if err != nil {
+		share.NewError(http.StatusInternalServerError, err.Error(), ctx)
+		return
+	}
+
+	share.NewResponse(http.StatusOK, "Task deleted successfully", nil, ctx)
+}
